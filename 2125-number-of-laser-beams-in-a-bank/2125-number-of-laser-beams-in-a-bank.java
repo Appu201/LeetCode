937cce8d0f2afd0d07f774e80c7db292ed32a422
Appu201 <appu200106@gmail.com>
2026-06-25 16:18:38 +0530

    Time: 9 ms (96.36%), Space: 47.8 MB (29.12%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int numberOfBeams(String[] bank) {
+        int result=0, prev=0;
+        for (int i=0; i<bank.length; i++) {
+            int count=0;
+            for (char c:bank[i].toCharArray()){
+                count += c - '0';
+                }
+            if (count==0) continue;
+            result+=prev*count;
+            prev=count;
+        }
+        return result;
+    }
+}
