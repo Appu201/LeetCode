d661839aff1ae50262e7893b1a596202984e1d12
Appu201 <appu200106@gmail.com>
2026-07-16 11:37:23 +0530

    Time: 2 ms (95.68%), Space: 48.7 MB (11.15%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,13 +1,32 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> m = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            int comp=target-nums[i];
-            if(m.containsKey(comp)){
-                return new int[]{m.get(comp),i+1};
+    public int[] twoSum(int[] numbers, int target) {
+        int start=0;
+        int end=numbers.length-1;
+        while(start<end){
+            int sum=numbers[start]+numbers[end];
+            if(sum==target){
+                return new int[]{start+1, end+1};
             }
-            m.put(nums[i],i+1);
+            else if(sum<target){
+                start++;
+            }
+            else{
+                end--;
+            }
         }
-        return new int[]{};
+        return new int[]{-1,-1};
     }
 }
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> m = new HashMap<>();
+//         for(int i=0; i<nums.length; i++){
+//             int comp=target-nums[i];
+//             if(m.containsKey(comp)){
+//                 return new int[]{m.get(comp),i+1};
+//             }
+//             m.put(nums[i],i+1);
+//         }
+//         return new int[]{};
+//     }
+// }
