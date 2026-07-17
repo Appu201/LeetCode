e7f0c85162c7c080afd1b3586b93a453b8ce4461
Appu201 <appu200106@gmail.com>
2026-07-17 14:24:52 +0530

    Time: 2 ms (19.58%), Space: 63.3 MB (12.41%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,8 +3,8 @@ class Solution {
         int n=nums.length;
         k=k%n;
 
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) {
             arr[(i+k)%n]=nums[i];
         }
         for(int i=0; i<n; i++) {
