d5b0af682e076dc89052fec6a149891989e71c87
Appu201 <appu200106@gmail.com>
2026-07-25 09:16:34 +0530

    Time: 1 ms (99.41%), Space: 43.1 MB (92.04%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        char[] chars=title.toCharArray();
+        int len=chars.length;
+        int i=0;
+        while(i<len) {
+            int start=i;
+            while (i<len && chars[i]!=' '){
+                chars[i]=Character.toLowerCase(chars[i]);
+                i++;
+            }
+            if ((i-start)>2) {
+                chars[start]=Character.toUpperCase(chars[start]);
+            }
+            i++;
+        }
+        return new String(chars);
+    }
+}
