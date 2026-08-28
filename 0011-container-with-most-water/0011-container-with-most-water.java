60ec803dc052971a8d543306fcfd9a02c20bd95e
Appu201 <appu200106@gmail.com>
2026-08-28 10:07:26 +0530

    Time: 5 ms (83.11%), Space: 77.4 MB (29.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -2,14 +2,13 @@ class Solution {
     public int maxArea(int[] a) {
         int l=0, r=a.length-1;
         int max=0;
-
-        while (l<r) {
-            int area = (r-l)*Math.min(a[l], a[r]);
-            max = Math.max(max, area);
-
-            if (a[l]<a[r]) {
+        while (l<r){
+            int area=(r-l)*Math.min(a[l], a[r]);
+            max=Math.max(max, area);
+            if(a[l]<a[r]){
                 l++;
-            } else {
+            } 
+            else{
                 r--;
             }
         }
