31fb5dbd3bebd967db9d1c6bd716eb189c7cc57f
Appu201 <appu200106@gmail.com>
2026-09-01 10:23:05 +0530

    Time: 5 ms (67.89%), Space: 46.3 MB (52.11%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numSub(String s) {
+        long ans=0;
+        long count=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='1'){
+                count++;
+            }
+            else{
+                ans+=count*(count+1)/2;
+                count=0;
+
+            }
+        }
+        ans+=count*(count+1)/2;
+        return (int)(ans%1000000007);
+    }
+}
