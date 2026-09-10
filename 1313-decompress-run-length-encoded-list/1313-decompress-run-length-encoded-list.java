97357445b2a728d45c1d7a0ba7f1a445f1e7a163
Appu201 <appu200106@gmail.com>
2026-09-10 16:14:23 +0530

    Time: 1 ms (81.2%), Space: 46.5 MB (74.07%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -1,12 +1,14 @@
 class Solution { 
-    public int[] decompressRLElist(int[] nums) { 
+    public int[] decompressRLElist(int[] nums) 
+    { 
         int ind=0; 
         int size=0; 
-        for(int i=0;i<nums.length;i+=2){ 
+        for(int i=0;i<nums.length;i+=2)
+        { 
             size+=nums[i]; 
         }
         int arr[]=new int[size]; 
-        for(int i=0;i<nums.length;i+=2){ 
+        for(int i=0;i<nums.length;i+=2){
             int val=nums[i+1]; 
             int freq=nums[i]; 
             for(int j=0;j<freq;j++){ 
@@ -16,3 +18,6 @@ class Solution {
         return arr; 
     } 
 }
+  
+
+   
